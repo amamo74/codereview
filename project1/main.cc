3c84dffd95a4bbe7358ad866cd62ab2e6fe7e6fa
amamo74 <47581065+amamo74@users.noreply.github.com>
2019-02-13 11:36:07 +0900

    Update main.cc

--- project1/main.cc
+++ project1/main.cc
@@ -36,7 +36,7 @@ int main(int argc, char* argv[]){
 	pushQueue(&priQueue,598);
 	pushQueue(&priQueue,302);
 
-	if( argc > 1 ){
+	if(argc > 1){ // remove the space before ')' and after '('
 		customNumber = atoi(argv[1]);
 		if( customNumber > -500 && customNumber < 2000 ){
 			pushQueue(&priQueue,customNumber);
